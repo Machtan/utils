a72125d8ff7d161affe4b45bf5f6f8b3d2698c28
Jakob Lautrup Nysom <jaln@itu.dk>
2015-02-28 21:51:07 +0100

    Improved dependency check for 'run'

--- build_fsharp.py
+++ build_fsharp.py
@@ -18,19 +18,24 @@ BIN_DIR = "MacOS"
 BINARIES_DIR = "binaries"
 LAUNCHER = "MonoMacLauncher"
 
+class SpecException(Exception):
+    """An exception for spec errors"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 def load_spec():
     """Attempts to load the local build specification"""
     if not os.path.exists(CONFIG):
-        raise Exception("Config file not found: Please create '{}' \
-            in your project directory".format(CONFIG))
+        raise SpecException("Config file not found: Please create" +
+            " '{}' in your project directory".format(CONFIG))
     else:
         spec = toml.load(CONFIG)
-        
+
         missing = []
         def ensure_not_missing(key):
             if not key in spec:
                 missing.append(key)
-        
+
         # Validate some shit
         ensure_not_missing("name")
         ensure_not_missing("main")
@@ -71,14 +76,14 @@ def bundle(clean=False, release=False):
     def remove_if_clean(path):
         if clean and os.path.exists(path):
             os.remove(path)
-    
+
     def ensure_dir(path):
         if not os.path.exists(path):
             os.mkdir(path)
-    
+
     # For the love of god
     pj = os.path.join
-    
+
     specs = load_spec()
     AUTHORS = specs['authors']
     NAME = specs['name']
@@ -91,18 +96,18 @@ def bundle(clean=False, release=False):
     EXE_NAME = NAME+".exe"
     APP_DIR = pj(BUILD_DIR, APP_NAME)
     ICON_NAME = specs['icon']
-    
+
     if not os.path.exists(os.path.join(BUILD_DIR, EXE_NAME)):
         return print("Executable not found, cannot bundle.")
-    
+
     # Begin!
     print("> Bundling app...")
     ensure_dir(APP_DIR)
-    
+
     # Create the contents dir
     CONTENTS_DIR = pj(APP_DIR, "Contents")
     ensure_dir(CONTENTS_DIR)
-    
+
     # Create the plist
     print("> Writing Property List...")
     with open("plist_template.txt", "r") as f:
@@ -119,7 +124,7 @@ def bundle(clean=False, release=False):
     remove_if_clean(plist_path)
     with open(plist_path, "w") as f:
         f.write(plist)
-        
+
     # Create the binary dir
     print("> Adding launcher...")
     LAUNCHER_DIR = pj(CONTENTS_DIR, "MacOS")
@@ -132,7 +137,7 @@ def bundle(clean=False, release=False):
                 os.remove(pj(LAUNCHER_DIR, file))
         launcher_src = pj(BINARIES_DIR, LAUNCHER)
         shutil.copy(launcher_src, launcher_path)
-    
+
     # Move the executable and dependencies
     print("> Adding binaries and assemblies... (.exe and .dll)")
     BUNDLE_DIR = pj(CONTENTS_DIR, "MonoBundle")
@@ -140,12 +145,12 @@ def bundle(clean=False, release=False):
     for file in os.listdir(BUNDLE_DIR): #  Remove the old exe
         if file.endswith(".exe"):
             os.remove(pj(BUNDLE_DIR, file))
-            
+
     exe_path = pj(BUNDLE_DIR, EXE_NAME)
     exe_src = pj(BUILD_DIR, EXE_NAME)
     remove_if_clean(exe_path)
     shutil.copy(exe_src, exe_path)
-    
+
     # Copy all dlls if this is not a release build
     if not release:
         print("> Copying binaries/assemblies...")
@@ -154,13 +159,13 @@ def bundle(clean=False, release=False):
             dep_src = pj(BINARIES_DIR, fullname)
             dep_dst = pj(BUNDLE_DIR, fullname)
             replace_if_newer(dep_src, dep_dst)
-    
+
         for mod in MODS:
             fullname = mod + ".dll"
             mod_src = pj(BUILD_DIR, fullname)
             mod_dst = pj(BUNDLE_DIR, fullname)
             replace_if_newer(mod_src, mod_dst)
-    
+
     # Move the Assets
     print("> Adding assets...")
     RES_DIR = pj(CONTENTS_DIR, "Resources")
@@ -168,7 +173,7 @@ def bundle(clean=False, release=False):
     icon_dst = pj(RES_DIR, ICON_NAME)
     icon_src = ICON_NAME
     replace_if_newer(icon_src, icon_dst)
-    
+
     ensure_dir(pj(RES_DIR, os.path.basename(ASSETS)))
     for dirpath, dirnames, filenames in os.walk(ASSETS):
         for dirname in dirnames:
@@ -184,7 +189,7 @@ def bundle(clean=False, release=False):
             file_dst = pj(RES_DIR, dirpath, filename)
             print(">>> file: src/dst '{}' / '{}'".format(file_src, file_dst))
             replace_if_newer(file_src, file_dst)
-    
+
     print("> Done!")
     print("> Saved to '{}'".format(APP_DIR))
 
@@ -192,20 +197,20 @@ def build(platform="mac", release=False):
     """Builds the F# program from the given specification dictionary"""
     specs = load_spec()
     ensure_build()
-    
+
     cmd = []
     cmd.append(BUILD_CMD)
     cmd.append("--nologo")
-    
+
     cmd.append("-I:{}".format(BINARIES_DIR))
     cmd.append("-I:{}".format(BUILD_DIR))
-    
+
     if platform == "mac":
         cmd.append("-d:TARGET_MAC")
-    
+
     for dep in specs['dependencies']:
         cmd.append("-r:{}.dll".format(dep))
-    
+
     # Build the submodules in the right order
     mods = specs['modules']
     submodules = []
@@ -217,7 +222,7 @@ def build(platform="mac", release=False):
         if recompile or newer(script, mod_dst):
             print("> Building '{}'...".format(script))
             recompile = True #  Make sure to also recompile the dependent scripts
-            
+
             # Compile the module
             mod_cmd = cmd + [script, "-a", "-o:{}".format(mod_dst)] + submodules
             print("$ {}".format(" ".join(mod_cmd)))
@@ -227,26 +232,26 @@ def build(platform="mac", release=False):
                 return exit_status
         else:
             print("> '{}' was built, skipping...".format(mod_name))
-            
+
         submodules.append("-r:{}".format(mod_name))
-    
+
     # Build the main program
     if release:
         cmd.append("--standalone")
-        
+
     out_file_name = specs['name'] + EXTENSION
     out_file = os.path.join(BUILD_DIR, out_file_name)
     cmd.append("-o:{}".format(out_file))
-    
+
     main_file = specs['main']
     cmd.append(main_file)
     cmd.extend(submodules)
-    
+
     print("$ {}".format(" ".join(cmd)))
     built = subprocess.call(cmd)
     if built: # Exit on failure
         return built
-    
+
     # Bundle the program if on mac
     if platform == "mac":
         bundled = bundle()
@@ -260,21 +265,25 @@ def run(force_recompile=False, args=[]):
     NAME = specs['name']
     target = os.path.join(BUILD_DIR, NAME + EXTENSION)
     main_file = specs['main']
-    
+
     # Compiled version exists
-    new_build = False
+    new_build = force_recompile
     if os.path.exists(target):
         # Check if it needs to be recompiled
-        if force_recompile or (os.stat(target).st_ctime <= os.stat(main_file).st_ctime):
-            new_build = True
+        checks = [main_file] + [m + ".fs" for m in specs["modules"]]
+        for check in checks:
+            if (os.stat(target).st_ctime < os.stat(check).st_ctime):
+                print("> New build!")
+                new_build = True
+                break
     else:
         new_build = True
-        
+
     if new_build:
         print("> Recompiling...")
         if build(): # Return not 0 => error
             return print("Error while building! Run failed.")
-    
+
     RUN_PATH = os.path.join(BUILD_DIR, NAME+".app", "Contents", BIN_DIR, NAME)
     cmd = [RUN_PATH] + args
     print("> Running...")
@@ -302,39 +311,41 @@ def main(args=sys.argv[1:]):
     # Prepare
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title = "commands")
-    
+
     build_desc = "Builds the project executable"
     build_parser = subparsers.add_parser("build", description=build_desc)
     build_parser.set_defaults(func=build)
-    
+
     run_desc = "Builds and runs the project executable"
     run_parser = subparsers.add_parser("run", description=run_desc)
-    run_parser.add_argument("-f", "--force_recompile", 
+    run_parser.add_argument("-f", "--force_recompile",
         action="store_true", default=False,
         help="Forces the program to recompile before running")
     run_parser.add_argument("args", nargs="*", default=[],
         help="Arguments for the executable")
     run_parser.set_defaults(func=run)
-    
+
     bundle_desc = "Bundles the project"
     bundle_parser = subparsers.add_parser("bundle", description=bundle_desc)
     bundle_parser.set_defaults(func=bundle)
-    
+
     clean_desc = "Cleans the target directory"
     clean_parser = subparsers.add_parser("clean", description=clean_desc)
     clean_parser.set_defaults(func=clean)
-    
+
     # Parse
     parsed = parser.parse_args()
-    
+
     # Run
     if not hasattr(parsed, "func"):
         parser.print_usage()
     else:
-        func = parsed.func
-        del(parsed.func)
-        func(**vars(parsed))
-    
+        try:
+            func = parsed.func
+            del(parsed.func)
+            func(**vars(parsed))
+        except SpecException as e:
+            print("! Error loading build file:\n{}".format(e))
+
 if __name__ == '__main__':
     main()
-    
